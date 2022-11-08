b13348e3807535d08738452650937485cf19025b
kylebaugh <kylebaugh1@gmail.com>
2022-11-08 15:35:35 -0700

    Kyle's version

---
+++ client/main.js
@@ -0,0 +1,105 @@
+const form = document.querySelector('form')
+
+const kayakArr = []
+
+const baseURL = `http://localhost:4004/api/houses`
+
+
+const getAllHouses = () => {
+    axios.get(baseURL)
+        .then((res) => {
+            displayHouses(res.data)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
+
+const deleteHouse = (id) => {
+    axios.delete(`${baseURL}/${id}`)
+        .then((res) => {
+            displayHouses(res.data)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
+
+const updateHouse = (id, type) => {
+    axios.put(`${baseURL}/${id}`, {type})
+        .then((res) => {
+            displayHouses(res.data)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
+
+const createHouse = (body) => {
+    axios.post(baseURL, body)
+        .then((res) => {
+            displayHouses(res.data)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
+
+const submitHandler = (e) => {
+    e.preventDefault()
+
+    let address = document.querySelector('#address')
+    let price = document.querySelector('#price')
+    let imageURL = document.querySelector('#img')
+
+    let bodyObj = {
+        address: address.value,
+        price: price.value, 
+        imageURL: imageURL.value
+    }
+
+    createHouse(bodyObj)
+
+    address.value = ''
+    price.value = ''
+    imageURL.value = ''
+}
+
+const createHouseCard = (house) => {
+    // locate section on HTML where we will be putting our movie card
+    const housesContainer = document.querySelector('#houses-container')
+
+    // Create a new HTML div element
+    const houseCard = document.createElement('div')
+
+    // Add the house-card class to the houseCard div we just created. This is mainly just for styling.
+    houseCard.classList.add('house-card')
+
+    // Define which additional HTML elements need to exist inside our houseCard div
+    houseCard.innerHTML = `
+    <img alt='house cover image' src=${house.imageURL} class="house-cover-image"/>
+    <p class="address">${house.address}</p>
+    <div class="btns-container">
+        <button onclick="updateHouse(${house.id}, 'minus')">-</button>
+        <p class="house-price">$${house.price}</p>
+        <button onclick="updateHouse(${house.id}, 'plus')">+</button>
+    </div>
+    <button onclick="deleteHouse(${house.id})">delete</button>
+    `
+
+    // Attach the houseCard div (with all of it's nested elements) to the housesContainer on our HTML
+    housesContainer.appendChild(houseCard)
+}
+
+const displayHouses = (arr) => {
+    const housesContainer = document.querySelector('#houses-container')
+
+    housesContainer.innerHTML = ``
+    for (let i = 0; i < arr.length; i++) {
+        createHouseCard(arr[i])
+    }
+}
+
+form.addEventListener('submit', submitHandler)
+
+getAllHouses()
